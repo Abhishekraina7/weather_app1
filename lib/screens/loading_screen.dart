1e711f0bb8b70cfc289a388431edb06e0d999098
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-11-27 10:12:45 +0530

    Added the code which ask for the user for his/her permission to access the location functionality on their mobile device.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,105 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void initState() {
+    super.initState();
+    _checkLocationStatus();
+  }
+  Future<void> _checkLocationStatus() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location service is enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled, show a dialog to enable them
+      _showLocationServiceDialog();
+      return;
+    }
+
+    // Request location permission
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Handle case when permission is denied
+      _showPermissionDeniedDialog();
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Handle case when permission is permanently denied
+      _showPermissionDeniedForeverDialog();
+    }
+  }
+  void _showLocationServiceDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Location Services Disabled'),
+        content: const Text('Please enable location services to use this app.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              // Open device settings to enable location services
+              Geolocator.openLocationSettings();
+            },
+            child: const Text('Open Settings'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showPermissionDeniedDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Location Permission Denied'),
+        content: const Text('Please grant location permission to use this app.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              // Request location permission again
+              _checkLocationStatus();
+            },
+            child: const Text('Retry'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showPermissionDeniedForeverDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Location Permission Denied'),
+        content: const Text('Please enable location permission in your device settings.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+              // Open app settings to enable location permission
+              Geolocator.openAppSettings();
+            },
+            child: const Text('Open App Settings'),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Future<void> getLocation()
+  async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,8 +107,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
-          child: Text('Get Location'),
+          child: const Text('Get Location'),
         ),
       ),
     );

b4a688c601f5472443d9ed4a46d8d67841ceff56
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-12-02 16:39:45 +0530

    Added the permission to access the internet funtionality for the android platform.

---
+++ lib/asyncConcept.dart
@@ -0,0 +1,59 @@
+
+
+
+import 'dart:io';
+
+void main()
+{
+  // justSomething();
+  justSomething2();
+}
+// Example of the synchoronour actions
+void justSomething() {
+  task1();
+  task2();
+  task3();
+}
+   void task1()
+  {
+    print('task 1 complete');
+  }
+   void task2()
+  {
+    // Until this this function gets completed the task 3 will not get executed
+    Duration time = Duration(seconds: 4);
+    sleep(time);
+    print('task 2 complete');
+  }
+  void task3()
+  {
+    print('task 3 complete');
+  }
+//Example of asynchronous actions
+void justSomething2() {
+  task4();
+  task5();
+  task6();
+}
+void task4()
+{
+
+  print('task 4 complete');
+}
+Future<String> task5 () async
+{
+  Duration seconds = Duration(seconds: 3);
+   late String result;
+  await Future.delayed(seconds,() {
+     result = 'task 5 data';
+    print('task 5 complete');
+    }
+  );
+  return result;
+  }
+
+void task6()
+{
+  print('task 6 completed');
+}
+
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -6,96 +6,17 @@ class Location{
   Location({this.latitude, this.longitude});
   late double ?latitude;
   late double ?longitude;
-  Future<void> _checkLocationStatus() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+   Future<void> getCurrentposition() async
+   {
+     try{
+       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+       latitude = position.latitude;
+       longitude = position.longitude;
+     }
+     catch(e)
+     {
+       print(e);
+     }
+   }
 
-    // Check if location service is enabled
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if(!serviceEnabled)
-    {
-      _showLocationServiceDialog();
-    }
-
-    // Request location permission
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Handle case when permission is denied
-      _showPermissionDeniedDialog();
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Handle case when permission is permanently denied
-      _showPermissionDeniedForeverDialog();
-    }
-  }
-  void _showLocationServiceDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Location Services Disabled'),
-        content: const Text('Please enable location services to use this app.'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              // Open device settings to enable location services
-              Geolocator.openLocationSettings();
-            },
-            child: const Text('Open Settings'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _showPermissionDeniedDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Location Permission Denied'),
-        content: const Text('Please grant location permission to use this app.'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              // Request location permission again
-              _checkLocationStatus();
-            },
-            child: const Text('Retry'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _showPermissionDeniedForeverDialog() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Location Permission Denied'),
-        content: const Text('Please enable location permission in your device settings.'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-              // Open app settings to enable location permission
-              Geolocator.openAppSettings();
-            },
-            child: const Text('Open App Settings'),
-          ),
-        ],
-      ),
-    );
-  }
-  Future<void> getLocation()
-  async {
-    try{
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);}
-    catch(e)
-    {
-      print(e);
-    }
-  }
 }

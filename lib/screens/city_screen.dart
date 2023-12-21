16af23296c607385b9ff2f8014611208f6f94de8
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-12-21 20:03:21 +0530

    Tried the Stack inside of the container. But the problem of renderflex because of the keyboard is not solved.

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -15,59 +15,63 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/city_background.jpg'),
-            fit: BoxFit.cover,
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // Background Image
+          Image.asset(
+            'images/city_background.jpg',
+            fit: BoxFit.cover, // or BoxFit.contain based on your preference
           ),
-        ),
-        constraints: const BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(
-                    Icons.arrow_back_ios,
-                    size: 50.0,
+          // Your Form Widgets
+          SafeArea(
+            child: Column(
+              children: <Widget>[
+                // Back Button
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.arrow_back_ios,
+                      size: 50.0,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                child: null,
-              ),
-               Container(
-                 padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                 child:  TextField(
-                   style: const TextStyle(
-                     color: Colors.black,
-                   ),
-                  decoration: kTextFieldInputDecoration,
-                   onChanged: (value)
-                   {
-                     cityName = value;
-                   },
-              ),
-               ),
-
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context, cityName);
-                },
-                child: const Text(
-                  'Get Weather',
-                  style: kButtonTextStyle,
+                // Padding for Form
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  child: null, // Add your form content here
                 ),
-              ),
-            ],
+                // Text Field
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextField(
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                    decoration: kTextFieldInputDecoration,
+                    onChanged: (value) {
+                      cityName = value;
+                    },
+                  ),
+                ),
+                // Get Weather Button
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, cityName);
+                  },
+                  child: const Text(
+                    'Get Weather',
+                    style: kButtonTextStyle,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
